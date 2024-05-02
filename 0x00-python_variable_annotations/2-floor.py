d29a418e0b43f7958971b902c710dff23d8a7154
Chikombe <chikombe.ecy@gmail.com>
2024-05-02 08:41:40 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+Module to define a function to return the floor of a float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The float number.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return math.floor(n)
