81788d202b02ec7213c4a7bdf0533b5b529be81a
Chikombe <chikombe.ecy@gmail.com>
2024-05-02 08:30:55 +0000

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module to define a function to add two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
