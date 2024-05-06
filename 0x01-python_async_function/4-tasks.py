998f0e68f70f24f31034b684ad33b34a8f048ce1
Chikombe <chikombe.ecy@gmail.com>
2024-05-06 14:06:59 +0000

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,20 +3,24 @@
 import asyncio
 from typing import List
 
+task_wait_random = __import__('3-tasks').task_wait_random
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Function that spawns task_wait_random n times
-    with the specified max_delay.
 
-    Args:
-        n (int): The number of times to spawn task_wait_random.
-        max_delay (int): The maximum delay in seconds for
-        each task_wait_random call.
-
-    Returns:
-        List[float]: The list of delays in ascending order.
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Async routine.
+        Args:
+            max_delay: integer argument.
+            n: integer argument.
+        Return:
+            List of all the delays random float.
     """
-    loop = asyncio.get_event_loop()
-    tasks = [loop.create_task(task_wait_random(max_delay)) for _ in range(n)]
-    delays = [await task for task in asyncio.as_completed(tasks)]
-    return sorted(delays)
+    delays: List[float] = []
+    orderList: List[float] = []
+
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    for o in asyncio.as_completed(delays):
+        orderList.append(await o)
+
+    return orderList
